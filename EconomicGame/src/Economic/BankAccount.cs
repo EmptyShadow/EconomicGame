9d428d276e26ee5c4c9a3689f46926f07e61f90a
EmptyShadow <dimon6407@gmail.com>
2017-11-08 18:37:52 +0300

    Implement BankAccount, add methods: Deposite, Withdraw

--- EconomicGame/src/Economic/BankAccount.cs
+++ EconomicGame/src/Economic/BankAccount.cs
@@ -11,5 +11,25 @@ namespace EconomicGame.src.Economic
     /// </summary>
     class BankAccount
     {
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        private decimal accBal;
+
+        public BankAccount()
+        {
+            accBal = 0;
+        }
+
+        /// <summary>
+        /// Получить баланс
+        /// </summary>
+        public decimal Balance
+        {
+            get
+            {
+                return accBal;
+            }
+        }
     }
 }
