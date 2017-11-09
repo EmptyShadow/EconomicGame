c5b2da9fad7c2d803ef4022db9c6c8d56deaf1de
EmptyShadow <dimon6407@gmail.com>
2017-11-09 10:22:24 +0300

    Реализован класс магазина: конструктор и определение стоимости

--- EconomicGame/src/Economic/Buildings/Market.cs
+++ EconomicGame/src/Economic/Buildings/Market.cs
@@ -6,11 +6,50 @@ using System.Threading.Tasks;
 
 namespace EconomicGame.src.Economic.Buildings
 {
+    /// <summary>
+    /// Магазин
+    /// </summary>
     class Market : Building
     {
-        public Market(string name, uint buildingPeriod, uint cost) : base(name, buildingPeriod, cost)
-        {
+        /// <summary>
+        /// Максимальный объем прибыли магазина
+        /// </summary>
+        uint maxProfit;
 
+        /// <summary>
+        /// Процент увеличения стоимости здания после постройки
+        /// </summary>
+        double procentCostInc;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Название</param>
+        /// <param name="buildingPeriod">Период строительства</param>
+        /// <param name="cost">Стоимость</param>
+        /// <param name="maxProfit">Максимальная прибыль</param>
+        /// <param name="procentCostInc">Процент увеличения стоимости здания</param>
+        public Market(string name, uint buildingPeriod, uint cost, uint maxProfit, double procentCostInc = 60) : base(name, buildingPeriod, cost)
+        {
+            this.maxProfit = maxProfit;
         }
+
+        /// <summary>
+        /// Получить стоимость здания
+        /// </summary>
+        /// <returns></returns>
+        public override uint GetCurCost()
+        {
+            // Если здание построенно
+            if (IsBuild())
+            {
+                // , то его стоимость на % больше чем его стоимость строительства
+                return (uint)((double)cost + procentCostInc / 100.0 * (double)cost);
+            }
+            //, иначе стоимость не построенного
+            return base.GetCurCost();
+        }
+
+
     }
 }
